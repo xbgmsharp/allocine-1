e3e96051069e12ec75347cc8a38d32d78e1b6b52
Yukilas <yukilas@gmail.com>
2013-01-12 13:54:50 +0100

    Adds the methods to research movies times.

--- allocine/api.py
+++ allocine/api.py
@@ -47,6 +47,16 @@ class AllocineAPI(object):
             params = dict(params, **{"filter": rtype})
         return self._send(url, params)
 
+    def _get_movies_times(self, *args, **kwargs):
+        url = "{0}{1}".format(self.base_url, "showtimelist")
+        params = self.base_params
+        for name, value in kwargs.items():
+            if value is not None:
+                if name == "zip_code":
+                    name = "zip"
+                params = dict(params, **{name: value})
+        return self._send(url, params)
+
     def search(self, query):
         return self._search(query, ["movie", "theater", "person", "news", "tvseries"])
 
@@ -91,3 +101,17 @@ class AllocineAPI(object):
 
     def get_movie_public_review(self, uid):
         return self._get_review(uid, "public")
+
+    def get_movies_times_in_city(self, zip_code, movie=None, date=None):
+        return self._get_movies_times(zip_code=zip_code, movie=movie, date=date)
+
+    def get_movies_times_in_theaters(self, theaters, movie=None, date=None):
+        return self._get_movies_times(theaters=theaters, movie=movie, date=date)
+
+    def get_movies_times_in_theater(self, theater_code=None, theater_name=None, movie=None, date=None):
+        if theater_name is not None:
+            return self._get_movies_times(theater_name=theater_name, movie=movie, date=date)
+        return self._get_movies_times(theaters=[theater_code], movie=movie, date=date)
+
+    def get_movies_times_near_coordinate(self, lat, long, radius, movie=None, date=None):
+        return self._get_movies_times(lat=lat, long=long, radius=radius, movie=movie, date=date)
