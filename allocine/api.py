17a98e2fd7ff357776c3b56e29382cf204715aea
Yukilas <yukilas@gmail.com>
2013-01-19 14:30:08 +0100

    Adds some documentation about the public methods.

--- allocine/api.py
+++ allocine/api.py
@@ -35,9 +35,11 @@ class AllocineAPI(object):
             "profile": profile,
             "filter": otype
         })
+        if media is not None:
+            params = dict(params, **{"mediafmt": media})
         return self._send(url, params)
 
-    def _get_review(self, uid, rtype=None):
+    def _get_reviews(self, uid, rtype=None):
         url = "{0}{1}".format(self.base_url, "reviewlist")
         params = dict(self.base_params, **{
             "code": uid,
@@ -61,49 +63,196 @@ class AllocineAPI(object):
         return self._send(url, params)
 
     def search(self, query):
+        """
+        Return the result of the search based on the specified query.
+
+        The search is done for movies, theaters, news, tv series and persons.
+
+        Param:
+            ``query`` -- The search string.
+
+        """
         return self._search(query, ["movie", "theater", "person", "news", "tvseries"])
 
     def search_movie(self, query):
+        """
+        Return the result of the *movies* search based on the specified query.
+
+        Param:
+            ``query`` -- The search string.
+
+        """
         return self._search(query, "movie")
 
     def search_theater(self, query):
+        """
+        Return the result of the *theaters* search based on the specified query.
+
+        Param:
+            ``query`` -- The search string.
+
+        """
         return self._search(query, "theater")
 
     def search_person(self, query):
+        """
+        Return the result of the *actors, directors, or producers* search based on the specified query.
+
+        Param:
+            ``query`` -- The search string.
+
+        """
         return self._search(query, "person")
 
     def search_news(self, query):
+        """
+        Return the result of the *news* search based on the specified query.
+
+        Param:
+            ``query`` -- The search string.
+
+        """
         return self._search(query, "news")
 
     def search_tvserie(self, query):
+        """
+        Return the result of the *tv series* search based on the specified query.
+
+        Param:
+            ``query`` -- The search string.
+
+        """
         return self._search(query, "tvseries")
 
     def get_infos(self, uid, otype, profile=None, media=None):
+        """
+        Return all the informations about the specified item.
+
+        Param:
+            ``uid`` -- The item unique ID.
+
+            ``otype`` -- The item type. Can be ``movie``, ``theater``,
+            ``person``, ``news`` or ``tvseries``.
+
+            ``profile`` -- (optional) The amount of returned informations.
+            Can be ``small``, ``medium`` or ``large`` (default is medium).
+
+            ``media`` -- (optional) The media format, see the media format's
+            page for all the informations.
+
+        """
         return self._get_infos(uid, otype, profile, media)
 
     def get_movie_infos(self, uid, profile=None, media=None):
+        """
+        Return all the informations about the specified movie.
+
+        Param:
+            ``uid`` -- The movie unique ID.
+
+            ``profile`` -- (optional) The amount of returned informations.
+            Can be ``small``, ``medium`` or ``large`` (default is medium).
+
+            ``media`` -- (optional) The media format, see the media format's
+            page for all the informations.
+
+        """
         return self._get_infos(uid, "movie", profile, media)
 
     def get_theater_infos(self, uid, profile=None, media=None):
+        """
+        Return all the informations about the specified theater.
+
+        Param:
+            ``uid`` -- The theater unique ID.
+
+            ``profile`` -- (optional) The amount of returned informations.
+            Can be ``small``, ``medium`` or ``large`` (default is medium).
+
+            ``media`` -- (optional) The media format, see the media format's
+            page for all the informations.
+
+        """
         return self._get_infos(uid, "theater", profile, media)
 
     def get_person_infos(self, uid, profile=None, media=None):
+        """
+        Return all the informations about the specified person.
+
+        Param:
+            ``uid`` -- The person unique ID.
+
+            ``profile`` -- (optional) The amount of returned informations.
+            Can be ``small``, ``medium`` or ``large`` (default is medium).
+
+            ``media`` -- (optional) The media format, see the media format's
+            page for all the informations.
+
+        """
         return self._get_infos(uid, "person", profile, media)
 
     def get_news_infos(self, uid, profile=None, media=None):
+        """
+        Return all the informations about the specified news.
+
+        Param:
+            ``uid`` -- The news unique ID.
+
+            ``profile`` -- (optional) The amount of returned informations.
+            Can be ``small``, ``medium`` or ``large`` (default is medium).
+
+            ``media`` -- (optional) The media format, see the media format's
+            page for all the informations.
+
+        """
         return self._get_infos(uid, "news", profile, media)
 
     def get_tvserie_infos(self, uid, profile=None, media=None):
+        """
+        Return all the informations about the specified tv serie.
+
+        Param:
+            ``uid`` -- The tv serie unique ID.
+
+            ``profile`` -- (optional) The amount of returned informations.
+            Can be ``small``, ``medium`` or ``large`` (default is medium).
+
+            ``media`` -- (optional) The media format, see the media format's
+            page for all the informations.
+
+        """
         return self._get_infos(uid, "tvseries", profile, media)
 
-    def get_movie_review(self, uid):
-        return self._get_review(uid, None)
+    def get_movie_reviews(self, uid):
+        """
+        Return the reviews made by the press and the community about
+        the specified movie.
 
-    def get_movie_press_review(self, uid):
-        return self._get_review(uid, "desk-press")
+        Param:
+            ``uid`` -- The movie unique ID.
 
-    def get_movie_public_review(self, uid):
-        return self._get_review(uid, "public")
+        """
+        return self._get_reviews(uid, None)
+
+    def get_movie_press_reviews(self, uid):
+        """
+        Return the reviews made by the press about the specified movie.
+
+        Param:
+            ``uid`` -- The movie unique ID.
+
+        """
+        return self._get_reviews(uid, "desk-press")
+
+    def get_movie_public_reviews(self, uid):
+        """
+        Return the reviews made by the community about the specified movie.
+
+        Param:
+            ``uid`` -- The movie unique ID.
+
+        """
+        return self._get_reviews(uid, "public")
 
     def get_movies_times_in_city(self, zip_code, movie=None, date=None):
         """
